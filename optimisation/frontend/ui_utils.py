7da778b5e781722d797aa24df6268dafb5c51dac
darklorddad <danieltiong000@gmail.com>
2025-05-14 12:46:52 +0800

    00006

--- optimisation/frontend/ui_utils.py
+++
@@ -1,3 +0,0 @@
-# This file is for UI utility functions specific to the Optimisation tab.
-# All functions from this file have been moved to optimisation/frontend/initial_optimisation_view_ui.py
-# This file can now be deleted.
